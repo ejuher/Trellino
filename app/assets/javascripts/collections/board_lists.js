c8472f37b4f6e005d28a3c4d52959875d1136a37
Erich Uher <ejuher@gmail.com>
2014-09-19 11:56:56 -0700

    added comparator in BoardLists collection

--- app/assets/javascripts/collections/board_lists.js
+++ app/assets/javascripts/collections/board_lists.js
@@ -2,9 +2,7 @@ TrelloClone.Collections.BoardLists = Backbone.Collection.extend({
 	model: TrelloClone.Models.List,
 
 	url: function () {
-		// OH NO! ON RAILS THE LISTS ARE NOT NESTED IN THE BOARDS SO THIS WILL
-		// NOT WORK!!!1 FUCKKKKK. Do I need to rewrite the routes? What lists do 
-		// I want here anyways? 
+		// will this work?? does this.board exist here???
 		return this.board.url() + "/lists"
 	},
 
@@ -12,6 +10,10 @@ TrelloClone.Collections.BoardLists = Backbone.Collection.extend({
 	// not currently being used. the board model calls this and passes in models 
 	// as an empty array.
 	initialize: function (models, options) {
-		this.board = options.board
+		this.board = options.board;
+	},
+
+	comparator: function (list) {
+		list.get('ord');		
 	}
 })
